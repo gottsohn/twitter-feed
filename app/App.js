13e6f5023058f83e4075fbf814fee1498d5f6823
Godson Ukpere <godson.ukpere@gmail.com>
2016-07-13 21:24:36 +0100

    feature(Base files): Add base files to application

--- app/App.js
+++ app/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import styles from './App.css';
+import classnames from 'classnames';
+import Header from './components/shared/Header.jsx';
+import Footer from './components/shared/Footer.jsx';
 
 export default class App extends React.Component {
+  static propTypes = {
+    children: React.PropTypes.element.isRequired
+  }
+
   constructor(props) {
     super(props);
-    this.state = {test: 'foo'};
   }
+
   render() {
     return (
-      <div className={styles.app}>
-        bar
-      </div>
+      <div className={styles.fullHeight}>
+      <Header />
+        <div className={classnames(styles.container, styles.fullHeight)}>
+          {this.props.children}
+        </div>
+      <Footer />
+    </div>
     );
   }
 }
