218afe8030d7ca0d92b0981bae94ca318dc8b68d
Godson Ukpere <godson.ukpere@gmail.com>
2016-07-13 21:22:29 +0100

    feature(actions): Add Flux actions

---
+++ app/actions/PublicActions.js
@@ -0,0 +1,39 @@
+import alt from '../lib/alt';
+
+class PublicActions {
+  static SETTINGS_KEY = 'settings';
+
+  setTitle(title) {
+    if (title) {
+      this.title(title);
+      window.document.title = title;
+    }
+
+    return true;
+  }
+
+  setDialogOpened(dialogOpened) {
+    this.dialogOpened(dialogOpened);
+    return true;
+  }
+
+  saveSettings(settings) {
+    window.localStorage.setItem('settings', JSON.stringify(settings));
+    this.settings(settings);
+    return true;
+  }
+
+  title(title) {
+    return title;
+  }
+
+  settings(settings) {
+    return settings;
+  }
+
+  dialogOpened(dialogOpened) {
+    return dialogOpened;
+  }
+}
+
+export default alt.createActions(PublicActions);
