c551c915c196c1c500646da23f56d77e801c06cf
Godson Ukpere <godson.ukpere@gmail.com>
2016-07-14 01:08:30 +0100

    chore(hound-ci): Resolve houndci issues

--- app/actions/SearchActions.js
+++ app/actions/SearchActions.js
@@ -3,25 +3,26 @@ import request from 'superagent';
 
 class SearchActions {
   search({text, language, geocode}) {
+    // Encode search query and concat with URL
     text = window.encodeURIComponent(text);
-    const url = `http://demo.suitepad.systems/1.1/search/tweets.json?q=${text}`+
-      `&lang=${language}&geocode=${geocode.lat},${geocode.lng},
-      ${geocode.radius}km`;
+    const url =
+      `http://demo.suitepad.systems/1.1/search/tweets.json?q=${text}` +
+      `&lang=${language}&geocode=${geocode.lat},${geocode.lng},` +
+      `${geocode.radius}km`;
 
+    // Make HTTP get request to Endpoint
     request
       .get(url)
       .set('Accept', 'application/json')
       .end((err, res) => {
-
-        console.log(res);
         if (!err) {
-          this.tweet(res);
+          this.searchSuccess(res.body);
         } else {
           this.searchError(err);
         }
       });
 
-      return true;
+    return true;
   }
 
   searchSuccess(tweets) {
